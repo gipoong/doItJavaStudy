c9b4ef550481ad7215cffd6acf0dbde419a60c3b
gipoong <gipoong@naver.com>
2025-06-29 21:13:51 +0900

    8장 소스 완료

---
+++ chapter8/PackageImprot_1.java
@@ -0,0 +1,12 @@
+package chapter8;
+
+
+public class PackageImprot_1 {
+    public static void main(String[] args) {
+        A a = new A();
+
+        System.out.println(a.m);
+        System.out.println(a.n);
+        a.print();
+    }
+}
